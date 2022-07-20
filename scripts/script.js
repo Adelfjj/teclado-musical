3c4f432fd5c543320af21c8c46d2f8f30dbe5357
Adelfjj <adelsonfranciscojj@outlook.com>
2022-07-19 21:15:44 -0300

    tocar as teclas musicais com as letras do teclado

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 let teclas = document.querySelectorAll('.teclado > .grupo > div');
 let audios = document.querySelectorAll('audio');
 
+const teclado = {
+    "ShiftLeft": "c2",
+    
+  };
+
+document.addEventListener("keyup", (e) => {
+    playSound(teclado[e.code]);
+})
+
+
 teclas.forEach(function(tecla){
     tecla.addEventListener("mousedown",(e)=>{
        let tecla = e.target;
